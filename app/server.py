0c9202985cdc01a8e2a2da9dbe29b027a589f059
kpaveliev <kpaveliev@gmail.com>
2022-08-05 12:27:18 +0300

    docker config

--- app/server.py
+++ app/server.py
@@ -1,10 +1,6 @@
 from flask import Flask
-from flask_restx import Api
 
 from app.views import start_bp, fight_bp
-
-# Initiate Api
-api = Api(title="SkyWars", doc="/docs")
 
 
 def create_app(config_obj):
--- run.py
+++ run.py
@@ -5,4 +5,4 @@ app = create_app(BaseConfig)
 app.config.from_envvar("APP_SETTINGS", silent=True)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port='25000')
+    app.run()
